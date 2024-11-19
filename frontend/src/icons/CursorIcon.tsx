89deee24d53017258a0133b073a8854e8148aa03
Ann Ruas <80757209+annRuas@users.noreply.github.com>
2024-11-18 23:15:53 -0300

    rfctr: making graph dynamic

--- frontend/src/icons/CursorIcon.tsx
+++ frontend/src/icons/CursorIcon.tsx
@@ -8,10 +8,7 @@ export const CursorIcon = (props: SvgProps) => (
     preserveAspectRatio="xMinYMin slice"
     viewBox="0 0 24.36 13.18"
   >
-    <Path
-      fill="#737373"
-      d="M15.89 12.008 1.28 6.796l14.514-5.475.096 10.686Z"
-    />
-    <Path fill="#737374" d="M16 5h9v3h-9z" />
+    <Path fill="#737373" d="M.7 6.802 15.99 1.034l.102 11.258L.7 6.802Z" />
+    <Path fill="#737374" d="M16 6h9v1h-9V6Z" />
   </Svg>
 );
