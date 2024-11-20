6a1c690f14cb440c65fd1c7c323443fb19d3b916
Ann Ruas <80757209+annRuas@users.noreply.github.com>
2024-11-20 15:17:33 -0300

    everyday everyhour

---
+++ frontend/src/components/user/ProfileNotLogged.tsx
@@ -0,0 +1,33 @@
+import { Link } from "expo-router"
+import { View, Text } from "react-native"
+import { Button } from "../Button"
+import { TextParagraph } from "../TextParagraph"
+
+
+export const ProfileNotLogged = () => {
+
+    /** @todo update the buttons */
+    return (
+        <View className="flex-1 mx-4 top-7 gap-y-3">
+
+            <View className="flex-row items-center gap-x-2">
+                <View className="bg-gray-500 w-12 h-12 rounded-full"></View>
+                <Text className="text-3xl font-bold text-center">Guest</Text>
+            </View>
+
+            <View className="my-5"/>
+            <TextParagraph className="text-xl text-center">
+                To have and personalize a profile, you must create one or log in your account.
+            </TextParagraph>
+            <View className="my-5"/>
+            <Link href="/Login" asChild>
+                <Button large>Log In</Button>
+            </Link>
+
+            <Link href="/Signup" asChild>
+                <Button large> Create Account</Button>
+            </Link>
+        </View>
+
+    )
+}
